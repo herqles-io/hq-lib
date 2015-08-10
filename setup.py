2a3328fe44cc37c46d029e9866f81ff64691a1df
Ryan Belgrave <rbelgrave@covermymeds.com>
2015-08-10 15:06:44 -0400

    update library requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.0',
     url='',
     include_package_data=True,
-    license='',
+    license='MIT',
     author='Ryan Belgrave',
     author_email='rbelgrave@covermymeds.com',
     description='Herqles Library',
@@ -14,9 +14,9 @@ setup(
     install_requires=[
         'pika==0.9.14',
         'enum34==1.0.4',
-        'sqlalchemy==1.0.4',
+        'sqlalchemy==1.0.8',
     ],
     extras_require={
-        'ldap': ['python-ldap==2.4.19']
+        'ldap': ['python-ldap==2.4.*']
     },
 )
